4082681b16c5ce1ca00cf10d12f34a4505d057b9
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-05-18 12:27:06 +0900

    fix: remove PWA for troubleshooting and delete broken profile directory

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,4 @@
 import type { NextConfig } from "next";
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: true, // Temporarily disabled
-  register: true,
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -24,4 +17,4 @@ const nextConfig: NextConfig = {
   turbopack: {},
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
